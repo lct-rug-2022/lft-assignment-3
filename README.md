85348c53779570ec0be237c9646cb7073f7645a6
Konstantin Chernyshev <k4black@ya.ru>
2022-09-28 21:31:47 +0200

    chore: add template and data splitting

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# lft-assignment-3
+# Learning From Data Assignment 3: TBA
+
+
+
+
+### Installation 
+
+Note: python 3.10 is required
+
+```shell
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install -U -r requirements.txt 
+```
+
+
+### Dataset split 
+
+Split default dataset (`datasets` folder `reviews.txt` file) with 0.7/0.15/0.15 as train/val/test sets.
+```shell
+python dataset_split.py
+```
+
+For more information please refer to the script help
+```shell
+python dataset_split.py --help
+```
+
+
+### Training model from scratch 
+
+Model with default dataset (`datasets` folder `train.txt` and `val.txt` files)
+```shell
+python train.py
+```
+
+For more information please refer to the script help
+```shell
+python train.py --help
+```
+
+
+### Testing trained model 
+
+Test trained model with default dataset (`datasets` folder `test.txt` files) and default model (`models` folder `model` file)
+```shell
+python test.py
+```
+
+For more information please refer to the script help
+```shell
+python test.py --help
+```
+
+
+
---
+++ dataset_split.py
@@ -0,0 +1,48 @@
+"""Dataset train/test split script"""
+
+from pathlib import Path
+
+import typer
+from sklearn.model_selection import train_test_split
+
+
+app = typer.Typer(add_completion=False)
+
+
+@app.command()
+def main(
+        dataset_file: Path = typer.Option('datasets/reviews.txt', exists=True, file_okay=True, dir_okay=False, readable=True, help='Dataset file to split'),
+        train_file: Path = typer.Option('datasets/train.txt', file_okay=True, dir_okay=False, writable=True, help='Train file write part of dataset'),
+        val_file: Path = typer.Option('datasets/val.txt', file_okay=True, dir_okay=False, writable=True, help='Validation file write part of dataset'),
+        test_file: Path = typer.Option('datasets/test.txt', file_okay=True, dir_okay=False, writable=True, help='Test file write part of dataset'),
+        val_size: float = typer.Option(0.15, help='Validation data size [0, 1]'),
+        test_size: float = typer.Option(0.15, help='Test data size [0, 1]'),
+):
+    """Split data on train, validation and test subsets."""
+
+    # read file
+    with open(dataset_file, encoding='utf-8') as f:
+        lines = list(f)
+
+    # split train/val/test set with given ratio
+    X_val_train, X_test = train_test_split(
+        lines,
+        test_size=test_size,
+        shuffle=False,
+        random_state=42
+    )
+    X_train, X_val = train_test_split(
+        X_val_train,
+        test_size=val_size*(1+test_size),
+        shuffle=True,
+        random_state=42
+    )
+
+    # save to the given files
+    for file, data in [(train_file, X_test), (val_file, X_val), (test_file, X_test)]:
+        with open(file, 'w', encoding='utf-8') as f:
+            f.write(''.join(data))
+
+
+if __name__ == "__main__":
+    app()
