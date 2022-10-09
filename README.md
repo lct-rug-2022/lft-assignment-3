37e15054a36a255021f48e52ddec8f8bda7b8718
Konstantin Chernyshev <k4black@ya.ru>
2022-10-09 22:49:25 +0200

    feat: add csv dataset

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ python -m pip install -U -r requirements.txt
 
 ### Dataset split 
 
-Split default dataset (`datasets` folder `reviews.txt` file) with 0.7/0.15/0.15 as train/val/test sets.
+Split default dataset (`datasets` folder `reviews.txt` file) with 0.7/0.15/0.15 as train/val/test sets as `csv` files.
 ```shell
 python dataset_split.py
 ```
@@ -26,10 +26,12 @@ For more information and additional parameters please refer to the script help
 python dataset_split.py --help
 ```
 
+The split dataset used for our experiments is uploaded to the git and can be found in `datasets` folder. 
+
 
 ### Training model from scratch 
 
-Model with default dataset (`datasets` folder `train.txt` and `val.txt` files)
+Model with default dataset (`datasets` folder `train.csv` and `val.csv` files)
 ```shell
 python train.py
 ```
@@ -39,10 +41,12 @@ For more information and additional parameters please refer to the script help
 python train.py --help
 ```
 
+The best model we trained can be found in the `models` folder.
+
 
 ### Testing trained model 
 
-Test trained model with default dataset (`datasets` folder `test.txt` files) and default model (`models` folder `model` file)
+Test trained model with default dataset (`datasets` folder `test.csv` files) and default model (`models` folder `model` file)
 ```shell
 python test.py
 ```
--- dataset_split.py
+++ dataset_split.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 from collections import Counter
 
+import pandas as pd
 import typer
 from sklearn.model_selection import train_test_split
 
@@ -10,12 +11,22 @@ from sklearn.model_selection import train_test_split
 app = typer.Typer(add_completion=False)
 
 
+def _line_to_dict(line: str) -> dict[str, str]:
+    split_line = line.split()
+    return {
+        'label': split_line[0],
+        'label_sentiment': split_line[1],
+        'id': split_line[2],
+        'text': ' '.join(split_line[3:]),
+    }
+
+
 @app.command()
 def main(
         dataset_file: Path = typer.Option('datasets/reviews.txt', exists=True, file_okay=True, dir_okay=False, readable=True, help='Dataset file to split'),
-        train_file: Path = typer.Option('datasets/train.txt', file_okay=True, dir_okay=False, writable=True, help='Train file write part of dataset'),
-        val_file: Path = typer.Option('datasets/val.txt', file_okay=True, dir_okay=False, writable=True, help='Validation file write part of dataset'),
-        test_file: Path = typer.Option('datasets/test.txt', file_okay=True, dir_okay=False, writable=True, help='Test file write part of dataset'),
+        train_file: Path = typer.Option('datasets/train.csv', file_okay=True, dir_okay=False, writable=True, help='Train file write part of dataset'),
+        val_file: Path = typer.Option('datasets/val.csv', file_okay=True, dir_okay=False, writable=True, help='Validation file write part of dataset'),
+        test_file: Path = typer.Option('datasets/test.csv', file_okay=True, dir_okay=False, writable=True, help='Test file write part of dataset'),
         val_size: float = typer.Option(0.15, help='Validation data size [0, 1]'),
         test_size: float = typer.Option(0.15, help='Test data size [0, 1]'),
 ):
@@ -44,14 +55,14 @@ def main(
 
     # save to the given files
     for file, data in [(train_file, X_train), (val_file, X_val), (test_file, X_test)]:
-        with open(file, 'w', encoding='utf-8') as f:
-            f.write(''.join(data))
+        # save as csv file
+        df_ = pd.DataFrame([_line_to_dict(i) for i in data])
+        df_.to_csv(file, index=False)
 
         # print stats
-        counter = Counter([i.split()[0] for i in data])
         print(f'{file}:')
-        for name, num in counter.items():
-            print(f'  {name}: {num}')
+        for k, v in df_['label'].value_counts().items():
+            print(f'    {k}: {v}')
         print(f'  total: {len(data)}')
 
 
